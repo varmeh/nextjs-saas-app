e66fa606426a4461515b1e8a89566568d994aab7
varmehta <varun@botiga.app>
2024-01-08 14:59:37 +0530

    daisyUI integration

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from 'next-themes'
 export default function Theme({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
-      attribute='class'
+      attribute='data-theme'
       storageKey='theme'
       themes={['light', 'dark']}
       defaultTheme='light'
