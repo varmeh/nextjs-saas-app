faab4d19d9694f17bf782e478cb96c027aac68c5
varmehta <varun@botiga.app>
2024-01-19 17:50:18 +0530

    fixinh header re-rendering

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -12,7 +12,7 @@ const HeaderLink = ({ href, children }: { href: string; children: React.ReactNod
 
 export default function Header() {
   return (
-    <header className='sticky top-0 w-full z-30'>
+    <header className='absolute w-full z-30'>
       <div className='max-w-6xl mx-auto px-4 sm:px-6'>
         <div className='flex items-center justify-between h-20'>
           {/* Site branding */}
