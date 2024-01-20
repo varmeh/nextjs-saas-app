6dc76f9a03d7e7120561f498ea6782118c430e69
varmehta <varun@botiga.app>
2024-01-20 16:55:22 +0530

    lint warnings fixes

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -2,7 +2,7 @@
 
 import AOSInitializer from '@/components/aos-initializer'
 
-export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+export default function DefaultLayout({ children }: { readonly children: React.ReactNode }) {
   return (
     <div className='max-w-6xl mx-auto px-4 sm:px-6'>
       {/* <PageIllustration /> */}
